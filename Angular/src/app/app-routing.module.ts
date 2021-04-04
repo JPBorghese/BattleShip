4ec6f1e6e7de92a3b62012251ec3ebc7f883aae2
JonathanBorghese <jpborghese@vt.edu>
2021-04-04 10:47:04 -0400

    created starting components

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+	{ path: '', component: WelcomeComponent },
+	{ path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
